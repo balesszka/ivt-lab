de9467339df8013befc10e74829938012711be17
balesszka <balintveraszto@gmail.com>
2019-04-26 13:12:42 +0200

    3th bug

--- src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
+++ src/main/java/hu/bme/mit/spaceship/TorpedoStore.java
@@ -30,8 +30,8 @@ public class TorpedoStore {
   }
 
   public boolean fire(int numberOfTorpedos){
-    if(numberOfTorpedos < 1 || numberOfTorpedos > this.torpedoCount){
-      new IllegalArgumentException("numberOfTorpedos");
+    if(numberOfTorpedos < 1 || numberOfTorpedos > this.torpedoCount){      
+throw	new IllegalArgumentException("numberOfTorpedos");
     }
 
     boolean success = false;

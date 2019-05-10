5635f087c27496d42f542eb7d38762cdd99f0a14
balesszka <36233582+balesszka@users.noreply.github.com>
2019-05-10 12:54:36 +0200

    Injextion + init in Test + verify

--- src/test/java/hu/bme/mit/spaceship/GT4500Test.java
+++ src/test/java/hu/bme/mit/spaceship/GT4500Test.java
@@ -8,33 +8,35 @@ import static org.mockito.Mockito.*;
 
 public class GT4500Test {
 
+  private DataAccess mockDA;
   private GT4500 ship;
 
   @BeforeEach
   public void init(){
-    this.ship = new GT4500();
+    mockDA = mock(DataAccess.class);
+    ship = new GT4500(mockDA);
   }
 
   @Test
   public void fireTorpedo_Single_Success(){
     // Arrange
-
+    when(mockDA.fireTorpedo(FiringMode.SINGLE)).thenReturn(50);
     // Act
-    boolean result = ship.fireTorpedo(FiringMode.SINGLE);
+    ship.fireTorpedo(FiringMode.SINGLE);
 
     // Assert
-    assertEquals(true, result);
+    verify(mockDA, times(1)).fireTorpedo(FiringMode.SINGLE);
   }
 
   @Test
   public void fireTorpedo_All_Success(){
     // Arrange
-
+    when(mockDA.fireTorpedo(FiringMode.ALL)).thenReturn(50);
     // Act
-    boolean result = ship.fireTorpedo(FiringMode.ALL);
+    ship.fireTorpedo(FiringMode.ALL);
 
     // Assert
-    assertEquals(true, result);
+    verify(mockDA, times(1)).fireTorpedo(FiringMode.ALL);
   }
 
 }
